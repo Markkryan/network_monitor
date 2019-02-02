9742068d037fd30d861b811a1b620372b5dfe813
Marc Bondoc <mrbondoc@ucalgary.ca>
2019-02-02 13:56:41 -0700

    Added new functionailty

--- network_monitor.py
+++ network_monitor.py
@@ -3,11 +3,19 @@ import logging
 
 def main():
     network_res = _retrive_resources()
-    print(network_res)
+    #print(network_res)
+    print(_retrieve_et_resource(network_res))
+    print(_retrieve_wifi_resource(network_res))
 
 def _retrive_resources():
-    network_res = psutil.net_io_counters(pernic=true)
+    network_res = psutil.net_io_counters(pernic=True)
     return network_res
+
+def _retrieve_et_resource(resource):
+    return resource["Ethernet"]
+
+def _retrieve_wifi_resource(resource):
+    return resource["Wi-Fi"]
 
 if __name__ == "__main__":
     main()
